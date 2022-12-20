5535a6db5bbd30270d8e0e5c2c3350da0e3e0c37
Gabriel Ravier <gabravier@gmail.com>
2022-12-20 22:36:28 +0100

    feat: Initial commit

---
+++ headers-from-svid.h
@@ -0,0 +1,28 @@
+Headers from SVID:
+siginfo.h: System V Release 4 (~1989/07/12)
+tiuser.h:
+sys/termiox.h:
+ticlts.h:
+ticots.h:
+ticotsord.h:
+sys/timod.h:
+sys/tiuser.h:
+sys/tihdr.h:
+sys/errno.h:
+sys/procset.h:
+sys/priocntl.h:
+sys/fppriocntl.h:
+sys/tspriocntl.h:
+pfmt.h:
+sys/sysmacros.h:
+termio.h:
+xti.h:
+sys/ksym.h:
+sys/lock.h:
+sys/mod.h:
+sys/mount.h:
+ustat.h:
+widec.h:
+synch.h:
+thread.h:
+
